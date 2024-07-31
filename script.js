532fba668de01791010d6bfc3c57867ffd19090a
Callaghanjake <jake_2024@hotmail.co.uk>
2024-07-31 12:14:53 +0100

    feat: Javascript validation completed with HTML warning pop up.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let form = document.querySelector('.signup-form');
 
-
 form.addEventListener('submit', (event) => {
     let password = document.querySelector('.password-input').value;
     let passwordConfirm = document.querySelector('.password-confirm-input').value;
+    let inputNotice = document.querySelector('.input-notice');
 
-    if (password === passwordConfirm) {
-        console.log('Passwords fine');
+    if (password === passwordConfirm && password !== '') {
         return true;
     } else if (password != passwordConfirm) {
         event.preventDefault();
-        console.log('Passwords dont match')
+        inputNotice.classList.add('password-notice');
+        inputNotice.textContent = 'The passwords do not match';
     } else if (password == '' || passwordConfirm == '') {
         event.preventDefault();
-        console.log('Passwords empty');
+        inputNotice.classList.add('password-notice');
+        inputNotice.textContent = 'The password field is empty';
     }
 });
