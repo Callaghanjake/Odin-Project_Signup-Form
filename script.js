bdac1cd40e9fd145ee515b851bf96cd7384ab3c2
Callaghanjake <jake_2024@hotmail.co.uk>
2024-08-01 09:42:32 +0100

    docs: add comments to JS file.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Variable to select the HTML form.
 let form = document.querySelector('.signup-form');
 
+// Event listener attached to form variable
 form.addEventListener('submit', (event) => {
+    // On form submit create variables for password inputs and validate span
     let password = document.querySelector('.password-input').value;
     let passwordConfirm = document.querySelector('.password-confirm-input').value;
     let inputNotice = document.querySelector('.input-notice');
 
+    // If the passwords are empty or do not match prevent form submit and validate
     if (password === passwordConfirm && password !== '') {
         return true;
     } else if (password != passwordConfirm) {
